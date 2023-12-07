56091bc088629846594daee5cfe0e7655f4529b1
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-12-06 22:28:30 -0600

    Update index.js

--- build-output-api/edge-functions/.vercel/output/functions/index.func/index.js
+++ build-output-api/edge-functions/.vercel/output/functions/index.func/index.js
@@ -1,3 +1,16 @@
-export default function index(request, event) {
-  return new Response(`Hello, from the Edge!`)
-}
+const { Redis } = require("@upstash/redis");
+
+module.exports = async (req, res) => {
+  /**
+   * Redis.fromEnv() will read the following from environment variables:
+   * - UPSTASH_REDIS_REST_URL
+   * - UPSTASH_REDIS_REST_TOKEN
+   */
+  const redis = Redis.fromEnv();
+  await redis.set("foo", "bar");
+  const bar = await redis.get("foo");
+
+  res.json({
+    body: `foo: ${bar}`,
+  });
+};
