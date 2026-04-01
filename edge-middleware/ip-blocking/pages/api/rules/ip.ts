63d95d66f0b4f0f476778a04729724cc65112ed9
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-01 15:08:55 -0500

    style: js-beautify formatting (edge-middleware/ip-blocking/pages/api/rules/ip.ts)

--- edge-middleware/ip-blocking/pages/api/rules/ip.ts
+++ edge-middleware/ip-blocking/pages/api/rules/ip.ts
@@ -1,6 +1,13 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { IP_RULES } from '@lib/rules/constants'
-import { upstashRest } from '@lib/upstash'
+import type {
+  NextApiRequest,
+  NextApiResponse
+} from 'next'
+import {
+  IP_RULES
+} from '@lib/rules/constants'
+import {
+  upstashRest
+} from '@lib/upstash'
 import getIP from '@lib/get-ip'
 
 // From https://stackoverflow.com/a/68104187
@@ -15,15 +22,25 @@ export default async function ip(req: NextApiRequest, res: NextApiResponse) {
         const action = req.body.action?.trim()
 
         if (!IP_REGEX.test(ip) || action !== 'block') {
-          return res.status(400).json({ error: { message: 'Invalid request' } })
+          return res.status(400).json({
+            error: {
+              message: 'Invalid request'
+            }
+          })
         }
 
-        const value = JSON.stringify({ action })
+        const value = JSON.stringify({
+          action
+        })
         const data = await upstashRest(['HSET', IP_RULES, ip, value])
-        return res.status(200).json({ done: data.result === 1 })
+        return res.status(200).json({
+          done: data.result === 1
+        })
       }
       case 'GET': {
-        const { result } = await upstashRest(['HGETALL', IP_RULES])
+        const {
+          result
+        } = await upstashRest(['HGETALL', IP_RULES])
         const rules = []
         // We'll send the real ip to show it in the input
         const myIp = getIP(req)
@@ -33,27 +50,42 @@ export default async function ip(req: NextApiRequest, res: NextApiResponse) {
           rules.push([result[i], JSON.parse(result[i + 1])])
         }
 
-        return res.status(200).json({ myIp, rules })
+        return res.status(200).json({
+          myIp,
+          rules
+        })
       }
       case 'DELETE': {
-        const { ip } = req.query
+        const {
+          ip
+        } = req.query
 
         if (!ip || typeof ip !== 'string') {
-          return res.status(400).json({ error: { message: 'Invalid request' } })
+          return res.status(400).json({
+            error: {
+              message: 'Invalid request'
+            }
+          })
         }
 
         const data = await upstashRest(['HDEL', IP_RULES, ip.trim()])
-        return res.status(200).json({ done: data.result === 1 })
+        return res.status(200).json({
+          done: data.result === 1
+        })
       }
       default:
         res.status(405).json({
-          error: { message: 'Method not allowed.' },
+          error: {
+            message: 'Method not allowed.'
+          },
         })
     }
   } catch (err) {
     console.error(err)
     return res.status(500).json({
-      error: { message: `An error ocurred, ${err}` },
+      error: {
+        message: `An error ocurred, ${err}`
+      },
     })
   }
 }
