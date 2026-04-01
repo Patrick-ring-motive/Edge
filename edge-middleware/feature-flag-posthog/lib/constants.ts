49ebf4ec6e87346ed73cc86fcf6032e96d072374
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-01 15:06:43 -0500

    style: js-beautify formatting (edge-middleware/feature-flag-posthog/lib/constants.ts)

--- edge-middleware/feature-flag-posthog/lib/constants.ts
+++ edge-middleware/feature-flag-posthog/lib/constants.ts
@@ -1,12 +1,12 @@
 export const POSTHOG_HOST = process.env.NEXT_PUBLIC_POSTHOG_HOST!
 
-export const POSTHOG_API_KEY = process.env.NEXT_PUBLIC_POSTHOG_PROJECT_API_KEY!
+  export const POSTHOG_API_KEY = process.env.NEXT_PUBLIC_POSTHOG_PROJECT_API_KEY!
 
-if (!POSTHOG_HOST) {
-  throw new Error(
-    'The environment variable NEXT_PUBLIC_POSTHOG_HOST is missing.'
-  )
-}
+    if (!POSTHOG_HOST) {
+      throw new Error(
+        'The environment variable NEXT_PUBLIC_POSTHOG_HOST is missing.'
+      )
+    }
 if (!POSTHOG_API_KEY) {
   throw new Error(
     'The environment variable NEXT_PUBLIC_POSTHOG_PROJECT_API_KEY is missing.'
@@ -22,6 +22,8 @@ export const FLAGS = {
   NEW_ABOUT_PAGE: 'New_About_Page',
   NEW_MARKETING_PAGE: 'New_Marketing_Page',
   NEW_PRODUCT_PAGE: 'New_Product_Page',
-} as const
+}
+as
+const
 
 export type Flags = typeof FLAGS[keyof typeof FLAGS]
