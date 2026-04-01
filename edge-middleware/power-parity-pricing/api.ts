15334342268a6658ff28faeb190d638f08b16b1f
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-01 15:10:17 -0500

    style: js-beautify formatting (edge-middleware/power-parity-pricing/api.ts)

--- edge-middleware/power-parity-pricing/api.ts
+++ edge-middleware/power-parity-pricing/api.ts
@@ -1,10 +1,14 @@
-import { PARITIES } from './constants'
-import { Country } from './types'
+import {
+  PARITIES
+} from './constants'
+import {
+  Country
+} from './types'
 
 export default {
   parity: {
-    list: async (): Promise<Country[]> => Object.keys(PARITIES) as Country[],
-    fetch: async (country: Country): Promise<number> => {
+    list: async (): Promise < Country[] > => Object.keys(PARITIES) as Country[],
+    fetch: async (country: Country): Promise < number > => {
       return PARITIES[country] || 10
     },
   },
