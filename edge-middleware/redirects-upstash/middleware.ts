7419d161b0c302d626733ae3c8128953b2e4496e
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-01 15:10:58 -0500

    style: js-beautify formatting (edge-middleware/redirects-upstash/middleware.ts)

--- edge-middleware/redirects-upstash/middleware.ts
+++ edge-middleware/redirects-upstash/middleware.ts
@@ -1,4 +1,6 @@
-import type { NextRequest } from 'next/server'
+import type {
+  NextRequest
+} from 'next/server'
 import redirects from '@lib/redirects'
 
 export async function middleware(req: NextRequest) {
