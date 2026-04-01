435dfc73a419ab6d0b59b6af942191dd4fb177eb
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-01 15:09:22 -0500

    style: js-beautify formatting (edge-middleware/jwt-authentication/middleware.ts)

--- edge-middleware/jwt-authentication/middleware.ts
+++ edge-middleware/jwt-authentication/middleware.ts
@@ -1,8 +1,13 @@
-import { type NextRequest, NextResponse } from 'next/server'
-import { verifyAuth } from '@lib/auth'
+import {
+  type NextRequest,
+  NextResponse
+} from 'next/server'
+import {
+  verifyAuth
+} from '@lib/auth'
 
 export const config = {
-  matcher: [ '/api/protected', '/protected' ],
+  matcher: ['/api/protected', '/protected'],
 }
 
 export async function middleware(req: NextRequest) {
@@ -15,8 +20,13 @@ export async function middleware(req: NextRequest) {
     // if this an API request, respond with JSON
     if (req.nextUrl.pathname.startsWith('/api/')) {
       return new NextResponse(
-        JSON.stringify({ 'error': { message: 'authentication required' } }),
-        { status: 401 });
+        JSON.stringify({
+          'error': {
+            message: 'authentication required'
+          }
+        }), {
+          status: 401
+        });
     }
     // otherwise, redirect to the set token page
     else {
