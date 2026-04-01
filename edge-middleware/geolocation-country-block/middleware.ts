3523d0281e1a124f06d4ddbe6bcd284f12efdb9c
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-01 15:07:11 -0500

    style: js-beautify formatting (edge-middleware/geolocation-country-block/middleware.ts)

--- edge-middleware/geolocation-country-block/middleware.ts
+++ edge-middleware/geolocation-country-block/middleware.ts
@@ -1,6 +1,10 @@
-import type { NextRequest } from 'next/server'
+import type {
+  NextRequest
+} from 'next/server'
 
-import { NextResponse } from 'next/server'
+import {
+  NextResponse
+} from 'next/server'
 
 // Block Austria, prefer Germany
 const BLOCKED_COUNTRY = 'AT'
