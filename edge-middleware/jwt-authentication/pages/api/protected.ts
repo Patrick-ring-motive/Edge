1d8f06e47dcced49eee790af808da04fb32acb13
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-01 15:09:31 -0500

    style: js-beautify formatting (edge-middleware/jwt-authentication/pages/api/protected.ts)

--- edge-middleware/jwt-authentication/pages/api/protected.ts
+++ edge-middleware/jwt-authentication/pages/api/protected.ts
@@ -1,10 +1,16 @@
-import { type NextRequest } from 'next/server'
-import { jsonResponse } from '@lib/utils'
+import {
+  type NextRequest
+} from 'next/server'
+import {
+  jsonResponse
+} from '@lib/utils'
 
 export const config = {
   runtime: 'edge',
 }
 
 export default async function protect(req: NextRequest) {
-  return jsonResponse(200, { success: true })
+  return jsonResponse(200, {
+    success: true
+  })
 }
