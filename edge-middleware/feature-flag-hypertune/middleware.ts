b89909abe38665ed75bccb68af5b078339690789
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-01 15:06:20 -0500

    style: js-beautify formatting (edge-middleware/feature-flag-hypertune/middleware.ts)

--- edge-middleware/feature-flag-hypertune/middleware.ts
+++ edge-middleware/feature-flag-hypertune/middleware.ts
@@ -1,4 +1,7 @@
-import { NextFetchEvent, NextRequest } from 'next/server'
+import {
+  NextFetchEvent,
+  NextRequest
+} from 'next/server'
 import hypertune from './lib/hypertune'
 
 export const config = {
@@ -10,10 +13,14 @@ export async function middleware(req: NextRequest, context: NextFetchEvent) {
 
   const rootNode = hypertune.root({
     context: {
-      user: { id: 'test', name: 'Test', email: 'test@test.com' },
+      user: {
+        id: 'test',
+        name: 'Test',
+        email: 'test@test.com'
+      },
     },
   })
-  const exampleFlag = rootNode.exampleFlag().get(/* fallback */ false)
+  const exampleFlag = rootNode.exampleFlag().get( /* fallback */ false)
   console.log('Edge Middleware flag:', exampleFlag)
 
   context.waitUntil(hypertune.flushLogs())
