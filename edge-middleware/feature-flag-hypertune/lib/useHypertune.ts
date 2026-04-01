6850277d4b4b3e5c62bd929237c07b8f4542f45e
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-01 15:06:18 -0500

    style: js-beautify formatting (edge-middleware/feature-flag-hypertune/lib/useHypertune.ts)

--- edge-middleware/feature-flag-hypertune/lib/useHypertune.ts
+++ edge-middleware/feature-flag-hypertune/lib/useHypertune.ts
@@ -1,9 +1,12 @@
-import React, { useEffect, useMemo } from 'react'
+import React, {
+  useEffect,
+  useMemo
+} from 'react'
 import hypertune from './hypertune'
 
 export default function useHypertune() {
   // Trigger a re-render when flags are updated
-  const [, setCommitHash] = React.useState<string | null>(
+  const [, setCommitHash] = React.useState < string | null > (
     hypertune.getCommitHash(),
   )
   useEffect(() => {
@@ -16,11 +19,15 @@ export default function useHypertune() {
   // Return the Hypertune root node initialized with the current user
   return useMemo(
     () =>
-      hypertune.root({
-        context: {
-          user: { id: 'test_id', name: 'Test', email: 'test@test.com' },
+    hypertune.root({
+      context: {
+        user: {
+          id: 'test_id',
+          name: 'Test',
+          email: 'test@test.com'
         },
-      }),
+      },
+    }),
     [],
   )
 }
