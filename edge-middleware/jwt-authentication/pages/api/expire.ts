2f40dc81f4b9211a2f918399f923e8142ad0fa68
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-01 15:09:28 -0500

    style: js-beautify formatting (edge-middleware/jwt-authentication/pages/api/expire.ts)

--- edge-middleware/jwt-authentication/pages/api/expire.ts
+++ edge-middleware/jwt-authentication/pages/api/expire.ts
@@ -1,6 +1,12 @@
-import { type NextRequest } from 'next/server'
-import { expireUserCookie } from '@lib/auth'
-import { jsonResponse } from '@lib/utils'
+import {
+  type NextRequest
+} from 'next/server'
+import {
+  expireUserCookie
+} from '@lib/auth'
+import {
+  jsonResponse
+} from '@lib/utils'
 
 export const config = {
   runtime: 'edge',
@@ -8,7 +14,13 @@ export const config = {
 
 export default async function expire(req: NextRequest) {
   if (req.method !== 'POST') {
-    return jsonResponse(405, { error: { message: 'Method not allowed' } })
+    return jsonResponse(405, {
+      error: {
+        message: 'Method not allowed'
+      }
+    })
   }
-  return expireUserCookie(jsonResponse(200, { success: true }))
+  return expireUserCookie(jsonResponse(200, {
+    success: true
+  }))
 }
