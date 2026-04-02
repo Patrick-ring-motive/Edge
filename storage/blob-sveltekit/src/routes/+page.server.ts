77681f5764b6fc2518d646f86b1310bd69ce104f
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 15:08:44 -0500

    style: js-beautify formatting (storage/blob-sveltekit/src/routes/+page.server.ts)

--- storage/blob-sveltekit/src/routes/+page.server.ts
+++ storage/blob-sveltekit/src/routes/+page.server.ts
@@ -1,16 +1,30 @@
-import { error } from '@sveltejs/kit'
-import { put } from '@vercel/blob'
+import {
+  error
+} from '@sveltejs/kit'
+import {
+  put
+} from '@vercel/blob'
 
 export const actions = {
-  upload: async ({ request }) => {
+  upload: async ({
+    request
+  }) => {
     const form = await request.formData()
     const file = form.get('image-upload') as File
 
     if (!file) {
-      throw error(400, { message: 'No file to upload.' })
+      throw error(400, {
+        message: 'No file to upload.'
+      })
     }
 
-    const { url } = await put(file.name, file, { access: 'public' })
-    return { uploaded: url }
+    const {
+      url
+    } = await put(file.name, file, {
+      access: 'public'
+    })
+    return {
+      uploaded: url
+    }
   },
 }
