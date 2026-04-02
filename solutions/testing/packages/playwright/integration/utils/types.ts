47b0cab807605de4881eef7a5ef661463ab7a394
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 15:07:17 -0500

    style: js-beautify formatting (solutions/testing/packages/playwright/integration/utils/types.ts)

--- solutions/testing/packages/playwright/integration/utils/types.ts
+++ solutions/testing/packages/playwright/integration/utils/types.ts
@@ -1,10 +1,12 @@
-import type { Response } from '@playwright/test'
+import type {
+  Response
+} from '@playwright/test'
 
 export enum HTTPMethod {
   Delete = 'DELETE',
-  Get = 'GET',
-  Patch = 'PATCH',
-  Post = 'POST',
+    Get = 'GET',
+    Patch = 'PATCH',
+    Post = 'POST',
 }
 
 type SearchParamsValue = '*' | string | RegExp
@@ -18,30 +20,32 @@ export interface SearchParamsProperties {
  * Allows for matching search params by a fixed value, any value (`*`),
  * or one that matches a regular expression.
  */
-type SearchParams = Record<string, SearchParamsValue | SearchParamsProperties>
+type SearchParams = Record < string, SearchParamsValue | SearchParamsProperties >
 
-/**
- * The configuration that can be used by the API to create mocks
- */
-interface ServiceConfig<T> {
-  path: string
-  method: HTTPMethod
-  pathParams?: Record<string, string>
-  searchParams?: SearchParams
-  status?: number
-  body: T
-}
+  /**
+   * The configuration that can be used by the API to create mocks
+   */
+  interface ServiceConfig < T > {
+    path: string
+    method: HTTPMethod
+    pathParams ? : Record < string,
+    string >
+    searchParams ? : SearchParams
+    status ? : number
+    body: T
+  }
 
 /**
  * Custom configuration that can be used by the test to extend the mock
  * before applying it. All keys are optional.
  */
-interface CreateMockConfig<T> {
-  pathParams?: Record<string, string>
-  searchParams?: SearchParams
-  status?: number
-  body?: T | ((body: T) => T)
-  times?: number
+interface CreateMockConfig < T > {
+  pathParams ? : Record < string,
+  string >
+  searchParams ? : SearchParams
+  status ? : number
+  body ? : T | ((body: T) => T)
+  times ? : number
 }
 
 /**
@@ -51,20 +55,18 @@ interface CreateMockConfig<T> {
  * @returns A new function that will implement the mock in
  * the test that calls it.
  */
-export type CreateMockFn = <T>(serviceConfig: ServiceConfig<T>) => (
-  config?: CreateMockConfig<T>
-) => Promise<
-  [
+export type CreateMockFn = < T > (serviceConfig: ServiceConfig < T > ) => (
+    config ? : CreateMockConfig < T >
+  ) => Promise < [
     /**
      * Returns an instance of `page.waitForResponse` which returns
      * a `Response` that matches the mock.
      */
     waitForResponse: (
-      matcher?: (response: Response) => boolean
-    ) => Promise<Response>,
+      matcher ? : (response: Response) => boolean
+    ) => Promise < Response > ,
     /**
      * Returns the response body that was used by the API mock.
      */
     getResponseBody: () => T
-  ]
->
+  ] >
