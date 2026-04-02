2594e566b180fcd944e1359478bc315e54936f89
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 15:06:57 -0500

    style: js-beautify formatting (solutions/testing/packages/playwright/integration/setup-fixture.ts)

--- solutions/testing/packages/playwright/integration/setup-fixture.ts
+++ solutions/testing/packages/playwright/integration/setup-fixture.ts
@@ -1,21 +1,39 @@
-import { baseFixture } from 'shared/base-fixture'
-import { createApiMocks, type MockApi } from './apis'
+import {
+  baseFixture
+} from 'shared/base-fixture'
+import {
+  createApiMocks,
+  type MockApi
+} from './apis'
 import createApiMockFn from './utils/create-mock-api'
-import { registerRouteHandler } from './utils/register-route-handler'
+import {
+  registerRouteHandler
+} from './utils/register-route-handler'
 
-type Extensions = { mockApi: MockApi; registerRouteHandler: void }
+type Extensions = {
+  mockApi: MockApi;registerRouteHandler: void
+}
 
-export const test = baseFixture.extend<Extensions>({
+export const test = baseFixture.extend < Extensions > ({
   registerRouteHandler: [
-    async ({ context, baseURL }, use) => {
+    async ({
+      context,
+      baseURL
+    }, use) => {
       await registerRouteHandler(context, baseURL)
       await use()
+    }, {
+      auto: true
     },
-    { auto: true },
   ],
-  mockApi: ({ page }, use) => use(createApiMocks(createApiMockFn(page))),
+  mockApi: ({
+    page
+  }, use) => use(createApiMocks(createApiMockFn(page))),
 })
 
 export type Test = typeof test
 
-export { expect } from '@playwright/test'
+export {
+  expect
+}
+from '@playwright/test'
