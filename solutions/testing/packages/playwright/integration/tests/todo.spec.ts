283c641f89bfb3ae5b57364ccd8667f49d6e8c78
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 15:07:02 -0500

    style: js-beautify formatting (solutions/testing/packages/playwright/integration/tests/todo.spec.ts)

--- solutions/testing/packages/playwright/integration/tests/todo.spec.ts
+++ solutions/testing/packages/playwright/integration/tests/todo.spec.ts
@@ -1,32 +1,56 @@
-import { Target } from '@applitools/eyes-playwright'
-import { todosBody } from 'integration/fixtures/api-todos/todos'
-import { test, expect } from 'integration/setup-fixture'
-import { authenticatedContext } from 'integration/utils/authenticated-context'
-import { TodoPage } from 'shared/pages/todo-page'
+import {
+  Target
+} from '@applitools/eyes-playwright'
+import {
+  todosBody
+} from 'integration/fixtures/api-todos/todos'
+import {
+  test,
+  expect
+} from 'integration/setup-fixture'
+import {
+  authenticatedContext
+} from 'integration/utils/authenticated-context'
+import {
+  TodoPage
+} from 'shared/pages/todo-page'
 
 // Add the user cookie to the browser context. The todos page
 // is behind authentication.
 test.use(authenticatedContext)
 
 test.describe('Todo Page', () => {
-  test('should be able to add todos', async ({ page, mockApi, eyes }) => {
-    const { todos } = todosBody
+  test('should be able to add todos', async ({
+    page,
+    mockApi,
+    eyes
+  }) => {
+    const {
+      todos
+    } = todosBody
     const todoPage = new TodoPage(page)
     const [waitForResponse] = await mockApi.todos.todo.get({
-      body: { todos: [] },
+      body: {
+        todos: []
+      },
     })
 
     // Navigate to the page and wait for a response from /api/todo
     await Promise.all([waitForResponse(), todoPage.goto()])
     await eyes.check('Todo page', Target.window().fully())
 
-    const { input, submitButton } = todoPage.getNewTodoForm()
+    const {
+      input,
+      submitButton
+    } = todoPage.getNewTodoForm()
     const todoItems = todoPage.getTodos()
 
     // Create 1st todo.
     const addFirstTodo = async () => {
       const [waitForResponse] = await mockApi.todos.todo.post({
-        body: { todos: [todos[0]] },
+        body: {
+          todos: [todos[0]]
+        },
       })
 
       await input.fill(todos[0].title)
@@ -38,7 +62,9 @@ test.describe('Todo Page', () => {
     // Create 2nd todo.
     const addSecondTodo = async () => {
       const [waitForResponse] = await mockApi.todos.todo.post({
-        body: { todos: todos.slice(0, 2) },
+        body: {
+          todos: todos.slice(0, 2)
+        },
       })
 
       await input.fill(todos[1].title)
@@ -60,9 +86,15 @@ test.describe('Todo Page', () => {
     page,
     mockApi,
   }) => {
-    const { todos } = todosBody
+    const {
+      todos
+    } = todosBody
     const todoPage = new TodoPage(page)
-    const [waitForResponse] = await mockApi.todos.todo.get({ body: { todos } })
+    const [waitForResponse] = await mockApi.todos.todo.get({
+      body: {
+        todos
+      }
+    })
 
     await Promise.all([waitForResponse(), todoPage.goto()])
 
@@ -74,22 +106,36 @@ test.describe('Todo Page', () => {
     mockApi,
     eyes,
   }) => {
-    const { todos } = todosBody
+    const {
+      todos
+    } = todosBody
     const todoPage = new TodoPage(page)
     const todo = todos[0]
     const [waitForTodos] = await mockApi.todos.todo.get({
-      body: { todos: [todo] },
+      body: {
+        todos: [todo]
+      },
     })
 
     await Promise.all([waitForTodos(), todoPage.goto()])
 
-    const { completeButton, undoButton } = todoPage.getTodoButtons()
+    const {
+      completeButton,
+      undoButton
+    } = todoPage.getTodoButtons()
 
     await expect(completeButton).toBeVisible()
 
     const [waitForResponse] = await mockApi.todos.todo.patch({
-      body: { todos: [{ ...todo, done: true }] },
-      searchParams: { id: todo.id },
+      body: {
+        todos: [{
+          ...todo,
+          done: true
+        }]
+      },
+      searchParams: {
+        id: todo.id
+      },
     })
 
     await Promise.all([waitForResponse(), completeButton.click()])
@@ -103,22 +149,39 @@ test.describe('Todo Page', () => {
     page,
     mockApi,
   }) => {
-    const { todos } = todosBody
+    const {
+      todos
+    } = todosBody
     const todoPage = new TodoPage(page)
-    const todo = { ...todos[0], done: true }
+    const todo = {
+      ...todos[0],
+      done: true
+    }
     const [waitForTodos] = await mockApi.todos.todo.get({
-      body: { todos: [todo] },
+      body: {
+        todos: [todo]
+      },
     })
 
     await Promise.all([waitForTodos(), todoPage.goto()])
 
-    const { completeButton, undoButton } = todoPage.getTodoButtons()
+    const {
+      completeButton,
+      undoButton
+    } = todoPage.getTodoButtons()
 
     await expect(undoButton).toBeVisible()
 
     const [waitForResponse] = await mockApi.todos.todo.patch({
-      body: { todos: [{ ...todo, done: false }] },
-      searchParams: { id: todo.id },
+      body: {
+        todos: [{
+          ...todo,
+          done: false
+        }]
+      },
+      searchParams: {
+        id: todo.id
+      },
     })
 
     await Promise.all([waitForResponse(), undoButton.click()])
@@ -126,17 +189,26 @@ test.describe('Todo Page', () => {
     await expect(completeButton).toBeVisible()
   })
 
-  test('should be able to remove todo items', async ({ page, mockApi }) => {
-    const { todos } = todosBody
+  test('should be able to remove todo items', async ({
+    page,
+    mockApi
+  }) => {
+    const {
+      todos
+    } = todosBody
     const todoPage = new TodoPage(page)
     const [waitForTodos] = await mockApi.todos.todo.get({
-      body: { todos },
+      body: {
+        todos
+      },
     })
 
     await Promise.all([waitForTodos(), todoPage.goto()])
 
     const todoItems = todoPage.getTodos()
-    const { removeButton } = todoPage.getTodoButtons(todoItems.first())
+    const {
+      removeButton
+    } = todoPage.getTodoButtons(todoItems.first())
 
     await expect(todoItems).toHaveCount(todos.length)
     await expect(todoItems.first()).toContainText(todos[0].title)
@@ -146,7 +218,9 @@ test.describe('Todo Page', () => {
       body: {
         todos: todos.slice(1),
       },
-      searchParams: { id: todos[0].id },
+      searchParams: {
+        id: todos[0].id
+      },
     })
 
     await Promise.all([waitForResponse(), removeButton.click()])
