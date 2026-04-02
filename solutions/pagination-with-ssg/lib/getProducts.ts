87e778920d568556fee4b51f4c0df1f6d34c1123
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 15:04:25 -0500

    style: js-beautify formatting (solutions/pagination-with-ssg/lib/getProducts.ts)

--- solutions/pagination-with-ssg/lib/getProducts.ts
+++ solutions/pagination-with-ssg/lib/getProducts.ts
@@ -14,5 +14,8 @@ export default async function getProducts({
   // Usually pagination is done by your DB, and the total is also known by the
   // DB, in this case we're using a demo json so things are simpler.
   const paginatedProducts = products.slice((page - 1) * limit, page * limit)
-  return { products: paginatedProducts, total: products.length }
+  return {
+    products: paginatedProducts,
+    total: products.length
+  }
 }
