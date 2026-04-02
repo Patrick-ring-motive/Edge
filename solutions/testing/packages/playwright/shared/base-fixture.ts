a0a019662857e102603d69f4e6194e5f50aa95da
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 15:07:28 -0500

    style: js-beautify formatting (solutions/testing/packages/playwright/shared/base-fixture.ts)

--- solutions/testing/packages/playwright/shared/base-fixture.ts
+++ solutions/testing/packages/playwright/shared/base-fixture.ts
@@ -1,14 +1,24 @@
-import { IS_CI, PAUSE_ON_FAILURE } from './constants'
+import {
+  IS_CI,
+  PAUSE_ON_FAILURE
+} from './constants'
 import pauseOnFailure from './fixtures/pause-on-failure'
-import { createUtils, type Utils } from './fixtures/utils'
-import { applitoolsTest } from './applitools'
+import {
+  createUtils,
+  type Utils
+} from './fixtures/utils'
+import {
+  applitoolsTest
+} from './applitools'
 import {
   BrowserType,
   DeviceName,
   ScreenOrientation,
 } from '@applitools/eyes-playwright'
 
-type BaseExtensions = { utils: Utils }
+type BaseExtensions = {
+  utils: Utils
+}
 
 const test = applitoolsTest({
   appName: 'Main Site',
@@ -27,8 +37,10 @@ const test = applitoolsTest({
   },
 })
 
-export const baseFixture = test.extend<BaseExtensions>({
-  utils: ({ page }, use) => use(createUtils(page)),
+export const baseFixture = test.extend < BaseExtensions > ({
+  utils: ({
+    page
+  }, use) => use(createUtils(page)),
 })
 
 export type Test = typeof baseFixture
