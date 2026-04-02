b292ebfe57abf61e728ada5e29a236e8b2bf7b8d
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 15:05:20 -0500

    style: js-beautify formatting (solutions/raw-body-functions/api/webhook.ts)

--- solutions/raw-body-functions/api/webhook.ts
+++ solutions/raw-body-functions/api/webhook.ts
@@ -1,5 +1,10 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-import type { Readable } from 'node:stream'
+import type {
+  VercelRequest,
+  VercelResponse
+} from '@vercel/node'
+import type {
+  Readable
+} from 'node:stream'
 
 export const config = {
   api: {
@@ -15,12 +20,14 @@ async function buffer(readable: Readable) {
   return Buffer.concat(chunks)
 }
 
-export default async function (req: VercelRequest, res: VercelResponse) {
+export default async function(req: VercelRequest, res: VercelResponse) {
   if (req.method === 'POST') {
     const buf = await buffer(req)
     const rawBody = buf.toString('utf8')
 
-    res.json({ rawBody })
+    res.json({
+      rawBody
+    })
   } else {
     res.setHeader('Allow', 'POST')
     res.status(405).end('Method Not Allowed')
