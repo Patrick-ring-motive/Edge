7283fd4b166ac06d0f9a909daf3b1a036ec3cddc
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 09:12:20 -0500

    style: js-beautify formatting (solutions/cron/pages/api/cron/[cron].ts)

--- solutions/cron/pages/api/cron/[cron].ts
+++ solutions/cron/pages/api/cron/[cron].ts
@@ -1,5 +1,10 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { kv } from '@vercel/kv'
+import {
+  NextRequest,
+  NextResponse
+} from 'next/server'
+import {
+  kv
+} from '@vercel/kv'
 
 export const config = {
   runtime: 'edge',
@@ -8,7 +13,9 @@ export const config = {
 export default async function handler(req: NextRequest) {
   const cron = req.nextUrl.pathname.split('/')[3]
   console.log(cron)
-  if (!cron) return new Response('No cron provided', { status: 400 })
+  if (!cron) return new Response('No cron provided', {
+    status: 400
+  })
   const response = await update(cron)
   return new NextResponse(JSON.stringify(response), {
     status: 200,
