d6db7d1a5eb97f14654ad628c1fdd9919497e71c
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 09:11:48 -0500

    style: js-beautify formatting (solutions/cms-contentstack-commerce/components/ui/index.ts)

--- solutions/cms-contentstack-commerce/components/ui/index.ts
+++ solutions/cms-contentstack-commerce/components/ui/index.ts
@@ -1,5 +1,20 @@
-export { default as Logo } from './Logo'
-export { default as Navbar } from './Navbar'
-export { default as Footer } from './Footer'
-export { default as Container } from './Container'
-export { default as UIComponent } from './UIComponent'
+export {
+  default as Logo
+}
+from './Logo'
+export {
+  default as Navbar
+}
+from './Navbar'
+export {
+  default as Footer
+}
+from './Footer'
+export {
+  default as Container
+}
+from './Container'
+export {
+  default as UIComponent
+}
+from './UIComponent'
