88b4d2ae170e12d6eaac154971624d8beee1a307
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 15:09:17 -0500

    style: js-beautify formatting (storage/kv-redis-sveltekit/src/routes/+page.server.ts)

--- storage/kv-redis-sveltekit/src/routes/+page.server.ts
+++ storage/kv-redis-sveltekit/src/routes/+page.server.ts
@@ -1,8 +1,10 @@
-import { kv } from '@vercel/kv'
+import {
+  kv
+} from '@vercel/kv'
 
 /** @type {import('./$types').PageLoad} */
 export async function load() {
-  const pageVisits = await kv.get<number>('pageVisits')
+  const pageVisits = await kv.get < number > ('pageVisits')
   await kv.set('pageVisits', (pageVisits || 0) + 1)
   const updatedPageVisits = await kv.get('pageVisits')
   return {
