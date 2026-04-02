9986b9e7103959bf895cbc15fe967c17fdb0b184
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 15:06:47 -0500

    style: js-beautify formatting (solutions/testing/packages/playwright/integration/apis/index.ts)

--- solutions/testing/packages/playwright/integration/apis/index.ts
+++ solutions/testing/packages/playwright/integration/apis/index.ts
@@ -1,10 +1,16 @@
-import { CreateMockFn } from 'integration/utils/types'
-import { createTodosMocks } from './todos'
-import { createUserMocks } from './user'
+import {
+  CreateMockFn
+} from 'integration/utils/types'
+import {
+  createTodosMocks
+} from './todos'
+import {
+  createUserMocks
+} from './user'
 
 export const createApiMocks = (createMockFn: CreateMockFn) => ({
   user: createUserMocks(createMockFn),
   todos: createTodosMocks(createMockFn),
 })
 
-export type MockApi = ReturnType<typeof createApiMocks>
+export type MockApi = ReturnType < typeof createApiMocks >
