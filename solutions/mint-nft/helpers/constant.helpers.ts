4effcf2f2a5aaf8f4e75396f353cab4dddd9feaf
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 09:14:03 -0500

    style: js-beautify formatting (solutions/mint-nft/helpers/constant.helpers.ts)

--- solutions/mint-nft/helpers/constant.helpers.ts
+++ solutions/mint-nft/helpers/constant.helpers.ts
@@ -3,7 +3,7 @@ type SupportedNetworks = "0x1" | "0x4"
 // this example would work on ethereum mainnet but Rinkeby is used to showcase. For main network change to "0x1"
 export const NETWORK_ID = "0x4"
 
-export const ChainLabelMap: Record<SupportedNetworks, string> = {
+export const ChainLabelMap: Record < SupportedNetworks, string > = {
   "0x1": 'Ethereum',
   "0x4": 'Rinkeby',
 }
