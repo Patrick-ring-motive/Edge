e9dac4037cfa2e1e6304544003327fd656246646
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 15:07:04 -0500

    style: js-beautify formatting (solutions/testing/packages/playwright/integration/utils/authenticated-context.ts)

--- solutions/testing/packages/playwright/integration/utils/authenticated-context.ts
+++ solutions/testing/packages/playwright/integration/utils/authenticated-context.ts
@@ -1,5 +1,10 @@
-import type { Fixtures, PlaywrightTestOptions } from '@playwright/test'
-import { getAuthState } from 'shared/utils/storage-state'
+import type {
+  Fixtures,
+  PlaywrightTestOptions
+} from '@playwright/test'
+import {
+  getAuthState
+} from 'shared/utils/storage-state'
 
 /**
  * An authenticated context for a spec.
@@ -7,6 +12,8 @@ import { getAuthState } from 'shared/utils/storage-state'
  * Note that one user is created per test file, per browser. Within the test
  * file, the same user (context) will be shared between tests.
  */
-export const authenticatedContext: Fixtures<{}, {}, PlaywrightTestOptions> = {
-  storageState: ({ baseURL }, use) => use(getAuthState(baseURL!)),
+export const authenticatedContext: Fixtures < {}, {}, PlaywrightTestOptions > = {
+  storageState: ({
+    baseURL
+  }, use) => use(getAuthState(baseURL!)),
 }
