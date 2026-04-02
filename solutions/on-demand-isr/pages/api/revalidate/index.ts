45f9a9f8596460b60bf0f7031adbe82d51f2e1f1
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 15:04:13 -0500

    style: js-beautify formatting (solutions/on-demand-isr/pages/api/revalidate/index.ts)

--- solutions/on-demand-isr/pages/api/revalidate/index.ts
+++ solutions/on-demand-isr/pages/api/revalidate/index.ts
@@ -1,9 +1,14 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type {
+  NextApiRequest,
+  NextApiResponse
+} from 'next'
 
 export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse
 ) {
   await res.revalidate('/')
-  return res.json({ revalidated: true })
+  return res.json({
+    revalidated: true
+  })
 }
