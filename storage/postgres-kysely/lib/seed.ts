08212405728501356fc9a21be4887dabdcea1ad1
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 15:09:50 -0500

    style: js-beautify formatting (storage/postgres-kysely/lib/seed.ts)

--- storage/postgres-kysely/lib/seed.ts
+++ storage/postgres-kysely/lib/seed.ts
@@ -1,4 +1,7 @@
-import { db, sql } from '@/lib/kysely'
+import {
+  db,
+  sql
+} from '@/lib/kysely'
 
 export async function seed() {
   const createTable = await db.schema
@@ -15,24 +18,20 @@ export async function seed() {
   console.log(`Created "users" table`)
   const addUsers = await db
     .insertInto('users')
-    .values([
-      {
+    .values([{
         name: 'Guillermo Rauch',
         email: 'rauchg@vercel.com',
-        image:
-          'https://images.ctfassets.net/e5382hct74si/2P1iOve0LZJRZWUzfXpi9r/9d4d27765764fb1ad7379d7cbe5f1043/ucxb4lHy_400x400.jpg',
+        image: 'https://images.ctfassets.net/e5382hct74si/2P1iOve0LZJRZWUzfXpi9r/9d4d27765764fb1ad7379d7cbe5f1043/ucxb4lHy_400x400.jpg',
       },
       {
         name: 'Lee Robinson',
         email: 'lee@vercel.com',
-        image:
-          'https://images.ctfassets.net/e5382hct74si/4BtM41PDNrx4z1ml643tdc/7aa88bdde8b5b7809174ea5b764c80fa/adWRdqQ6_400x400.jpg',
+        image: 'https://images.ctfassets.net/e5382hct74si/4BtM41PDNrx4z1ml643tdc/7aa88bdde8b5b7809174ea5b764c80fa/adWRdqQ6_400x400.jpg',
       },
       {
         name: 'Steven Tey',
         email: 'stey@vercel.com',
-        image:
-          'https://images.ctfassets.net/e5382hct74si/4QEuVLNyZUg5X6X4cW4pVH/eb7cd219e21b29ae976277871cd5ca4b/profile.jpg',
+        image: 'https://images.ctfassets.net/e5382hct74si/4QEuVLNyZUg5X6X4cW4pVH/eb7cd219e21b29ae976277871cd5ca4b/profile.jpg',
       },
     ])
     .execute()
